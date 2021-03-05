e899587d29a80788759ae4bb83306274e831766d
Kashslesh <46224990+Kashslesh@users.noreply.github.com>
2021-03-05 10:18:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php
+require './elements/header.php';
+require './elements/contenus.php';
+require './elements/insertion.php';
+require '.elements/supprimer.php';
+require './elements/footer.php';
+?>
+
